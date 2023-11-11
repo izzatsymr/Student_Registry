05f22d51838e0b333279d6882d5c92a57e647772
izzat syamir <112875861+izzatsymr@users.noreply.github.com>
2023-11-12 01:48:23 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,23 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\StudentController;
 
+Route::get('/callback', function (Request $request) {
+    $http = new GuzzleHttp\Client;
+
+    $response = $http->post('http://127.0.0.1:8000/outh/token',[
+        'form_params' => [
+            'grant_type'=> 'password',
+            'client_id'=>2,
+            'client_secret'=> 'cwu3Q3aBwxjtQONwPP0Zbz1XzwKsPDfZuHJ0xvg1',
+            'username'=> 'test@user.com',
+            'password'=> 'user',
+            'scope'=>''
+        ]
+    ]);
+
+    return json_decode((string) $response->getBody(),true);
+});
+
 Route::group(['middleware' => 'auth:api'], function () {
     Route::apiResource('students', StudentController::class);
     Route::post('students/search', [StudentController::class, 'search']);
